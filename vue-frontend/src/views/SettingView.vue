cf60253ee1fa8359627813e1c22008a939c0fd90
SOL CHOI <habrush3024@gmail.com>
2022-06-06 10:34:26 +0900

    setting update

--- vue-frontend/src/views/SettingView.vue
+++ vue-frontend/src/views/SettingView.vue
@@ -1,35 +1,40 @@
 <template>
-  <div class="card bg-secondary">
-    <div class="card-body" id="font">
+<div class="row">
+  <div class="col-1"></div>
+  <div class="col-10">
+    <div id="font">
       <br>
-      글꼴 설정
+   <div style="font-weight:bold;margin-bottom:-12%">   글꼴 설정</div>
       <br>
-        <input type="radio"  id="one" class="form-check-input" style="margin:3%" value='BareunDotum' v-model="font" checked>
+        <input type="radio"  id="one" class="form-check-input" style="margin:3% " value='BareunDotum' v-model="font" checked>
         <label class="form-check-label"  for="one">돋움체</label> <br>
         <input type="radio" id="two" class="form-check-input" style="margin:3%" value='ChosunGu' v-model="font">
         <label class="form-check-label"  for="two">굴림체</label>
     </div>
     <hr>
-    <div class="card-body" id="font-size">글자 크기 설정
+    <div id="font-size">
+      <div style="font-weight:bold;margin-bottom:-10%">글자 크기 설정</div>
         <br>
-        <div class="col-12">
-            <button id="minus" @click="font_size--">-</button>
-            <input v-model="font_size" placeholder="300" size="13%" style="width:60%;">%
-            <button id="minus" @click="font_size++">+</button>
-        </div>
+            <button id="minus" class="btn btn-secondary col-1" @click="font_size--">-</button>
+            <input v-model="font_size" class="col-8"  placeholder="기본 300">%
+            <button id="minus" class="btn btn-secondary col-1" @click="font_size++">+</button>
     </div>
     <hr>
-    <div class="card-body" id="dark">다크 모드 설정
+    <div id="dark">
+      <div style="font-weight:bold;margin-bottom:-13%">다크 모드 설정</div>
         <br>
         <input class="form-check-input" type="radio" id="on" value=1 v-model="dark">
         <label class="form-check-label" style="margin:1%;" for="on">ON</label><br>
         <input class="form-check-input" type="radio" id="off" value=0 v-model="dark" checked>
         <label class="form-check-label" style="margin:1%;" for="off">OFF</label>
     </div>
-   <button class="btn btn-dark text-white btn-lg"
-   style="font-weight:bold; font-size:120%;"
-    @click="change">확인</button>
   </div>
+  </div>
+  <div class="col-1"></div>
+   <button class="btn btn-light btn-outline-dark col-12"
+   style="font-weight:bold; font-size:120%; margin:0 auto;"
+    @click="change">확인</button>
+  
 </template>
 
 <script>
